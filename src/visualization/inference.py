92fa78bfa955237c9ffe4b15800b066b98550258
gcleroux <guicleroux@icloud.com>
2021-06-17 16:40:55 -0400

    Added inference testing for DE:TR

--- src/visualization/inference.py
+++ src/visualization/inference.py
@@ -15,11 +15,12 @@ from PIL import Image, ImageDraw, ImageFont
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 from typing import Tuple
+from zipfile import ZipFile
 
 from src.utils.constants import CLASS_DICT, FONT_PATH, MODELS_PATH, RESIZED_PATH, RAW_PATH, IMAGE_EXT
-from src.utils.helper_functions import filter_by_nms, filter_by_score
+from src.utils.helper_functions import filter_by_nms, filter_by_score, format_detr_outputs
 
-
+@torch.no_grad()
 def save_test_images(model_file_name: str,
                      data_loader: DataLoader,
                      iou_threshold: float,
@@ -43,9 +44,20 @@ def save_test_images(model_file_name: str,
     # Declare progress bar
     pbar = tqdm(total=len(data_loader), leave=False, desc='Inference Test')
 
-    # Load saved model
-    model = torch.load(f'{MODELS_PATH}{model_file_name}')
+    # Loading the model 
+    with ZipFile(f'{MODELS_PATH}{model_file_name}', 'r') as zipObj:
+        model_type = str(zipObj.read('archive/data.pkl')).split("\\n")[1]
 
+        if model_type == 'DETR':
+            model = torch.hub.load('facebookresearch/detr',
+                                    'detr_resnet50', pretrained=False, num_classes=len(CLASS_DICT))
+
+            model.load_state_dict(torch.load(
+                f'{MODELS_PATH}{model_file_name}').state_dict())
+            
+        else:
+            model = torch.load(f'{MODELS_PATH}{model_file_name}')
+                
     model.to(device)
 
     # Put the model into eval() mode for inference
@@ -54,68 +66,71 @@ def save_test_images(model_file_name: str,
     # Declare the font object to write the confidence scores on the images
     font = ImageFont.truetype(FONT_PATH, 12)
 
-    # Deactivate the autograd engine
-    with torch.no_grad():
-        # Loop through each batch in the data loader
-        for batch_no, (images, targets) in enumerate(data_loader):
-            # Get current batch indices
-            indices = range(data_loader.batch_size * batch_no,
-                            min(data_loader.batch_size * (batch_no + 1), len(data_loader.dataset)))
+    # Loop through each batch in the data loader
+    for batch_no, (images, targets) in enumerate(data_loader):
+        # Get current batch indices
+        indices = range(data_loader.batch_size * batch_no,
+                        min(data_loader.batch_size * (batch_no + 1), len(data_loader.dataset)))
 
-            # Send images to the device
-            images = torch.stack(images).to(device)
+        # Send images to the device
+        images = torch.stack(images).to(device)
 
-            # Perform a forward pass and obtain bounding boxes predictions
-            preds = model(images)
+        # Perform a forward pass and obtain bounding boxes predictions
+        preds = model(images)
 
-            # Filter predicted bounding boxes by using non-maximum suppression
-            preds = filter_by_nms(preds, iou_threshold)
+        if model_type == 'DETR':
+            target_sizes = torch.stack(
+                [torch.tensor([image_size, image_size]) for _ in range(data_loader.batch_size)], dim=0)
+            preds = format_detr_outputs(preds, target_sizes)
 
-            # Filter predicted bounding boxes by using a confidence score threshold
-            preds = filter_by_score(preds, score_threshold)
+        # Filter predicted bounding boxes by using non-maximum suppression
+        preds = filter_by_nms(preds, iou_threshold)
 
-            # Load images in the current batch
-            images_raw = [Image.open(image_paths_raw[index]) for index in indices]
+        # Filter predicted bounding boxes by using a confidence score threshold
+        preds = filter_by_score(preds, score_threshold)
 
-            # Loop through each image in the current batch
-            for index, image, image_raw, target, pred in zip(indices, images, images_raw, targets, preds):
-                image_resized = image_raw.copy()
+        # Load images in the current batch
+        images_raw = [Image.open(image_paths_raw[index]) for index in indices]
 
-                # Check if the original size is larger than the maximum image size
-                if image_size < image_raw.size[0] or image_size < image_raw.size[1]:
-                    # Downsize the image using the thumbnail method
-                    image_resized.thumbnail((image_size, image_size),
-                                            resample=Image.BILINEAR,
-                                            reducing_gap=2)
+        # Loop through each image in the current batch
+        for index, image, image_raw, target, pred in zip(indices, images, images_raw, targets, preds):
+            image_resized = image_raw.copy()
 
-                    # Calculate the downsize ratio
-                    downsize_ratio = image_resized.size[0] / image_raw.size[0]
-                else:
-                    downsize_ratio = 1
+            # Check if the original size is larger than the maximum image size
+            if image_size < image_raw.size[0] or image_size < image_raw.size[1]:
+                # Downsize the image using the thumbnail method
+                image_resized.thumbnail((image_size, image_size),
+                                        resample=Image.BILINEAR,
+                                        reducing_gap=2)
 
-                # Calculate the x and y offsets at which the downsized image needs to be pasted (to center it)
-                x_offset = int((image_size - image_resized.size[0]) / 2)
-                y_offset = int((image_size - image_resized.size[1]) / 2)
+                # Calculate the downsize ratio
+                downsize_ratio = image_resized.size[0] / image_raw.size[0]
+            else:
+                downsize_ratio = 1
 
-                target = resize_box_coord(target, downsize_ratio, x_offset, y_offset)
-                pred = resize_box_coord(pred, downsize_ratio, x_offset, y_offset)
+            # Calculate the x and y offsets at which the downsized image needs to be pasted (to center it)
+            x_offset = int((image_size - image_resized.size[0]) / 2)
+            y_offset = int((image_size - image_resized.size[1]) / 2)
 
-                # Declare an ImageDraw object
-                draw = ImageDraw.Draw(image_raw)
+            target = resize_box_coord(target, downsize_ratio, x_offset, y_offset)
+            pred = resize_box_coord(pred, downsize_ratio, x_offset, y_offset)
 
-                # Draw ground truth bounding boxes
-                draw_boxes(draw, target, 'green', 3, font, (255, 255, 0, 0))
+            # Declare an ImageDraw object
+            draw = ImageDraw.Draw(image_raw)
 
-                # Draw predicted bounding boxes
-                draw_boxes(draw, pred, 'red', 3, font, (255, 255, 255, 0))
+            # Draw ground truth bounding boxes
+            draw_boxes(draw, target, 'green', 3, font, (255, 255, 0, 0))
 
-                # Save the image
-                image_raw.save(f'{save_path}'
-                               f'{data_loader.dataset.image_paths[index].rsplit("/", 1)[-1].split(".", 1)[0]}'
-                               f'.{IMAGE_EXT}')
+            # Draw predicted bounding boxes
+            draw_boxes(draw, pred, 'red', 3, font, (255, 255, 255, 0))
 
-            # Update the progress bar
-            pbar.update()
+            # Save the image
+            image_raw.save(f'{save_path}'
+                            f'{data_loader.dataset.image_paths[index].rsplit("/", 1)[-1].split(".", 1)[0]}'
+                            f'.{IMAGE_EXT}')
+
+        # Update the progress bar
+        pbar.update()
 
     # Close the progress bar
     pbar.close()
