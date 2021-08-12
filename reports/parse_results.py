5449ac87e11982228e2c6838f6107161d9e07a60
sgiardl <sgiardleroux@gmail.com>
2021-08-12 15:29:10 -0400

    Modified results parsing and experiment script names

--- reports/parse_results.py
+++ reports/parse_results.py
@@ -85,33 +85,33 @@ def parse_results(saved_models_path: str, log_path: str) -> pd.DataFrame:
     df = pd.DataFrame(columns=columns)
 
     for subdir, dirs, files in os.walk(saved_models_path):
+        files = [file for file in files if file != '.gitkeep']
         for file in tqdm(files, desc='Parsing results...'):
-            if file != '.gitkeep':
-                save_state = torch.load(saved_models_path + file, map_location=torch.device('cpu'))
-                model = save_state['args_dict']['model'].split('_')[0]
-                lr = format(save_state['args_dict']['learning_rate'], '.0E')
-                wd = format(save_state['args_dict']['weight_decay'], '.0E')
-                da = save_state['args_dict']['data_aug']
+            save_state = torch.load(saved_models_path + file, map_location=torch.device('cpu'))
+            model = save_state['args_dict']['model'].split('_')[0]
+            lr = format(save_state['args_dict']['learning_rate'], '.0E')
+            wd = format(save_state['args_dict']['weight_decay'], '.0E')
+            da = save_state['args_dict']['data_aug']
 
-                event_acc = EventAccumulator(log_path + file)
-                event_acc.Reload()
-                # scalars = event_acc.Tags()['scalars']
+            event_acc = EventAccumulator(log_path + file)
+            event_acc.Reload()
+            # scalars = event_acc.Tags()['scalars']
 
-                results_list = []
+            results_list = []
 
-                for key, value in scalar_dict.items():
-                    time, step, val = zip(*event_acc.Scalars(key))
-                    best_ap = round(val[0] * 100, 1)
+            for key, value in scalar_dict.items():
+                time, step, val = zip(*event_acc.Scalars(key))
+                best_ap = round(val[0] * 100, 1)
 
-                    results_list.append(best_ap)
+                results_list.append(best_ap)
 
-                df = df.append(pd.DataFrame([[model, lr, wd, da, *results_list]], columns=df.columns))
+            df = df.append(pd.DataFrame([[model, lr, wd, da, *results_list]], columns=df.columns))
 
-                run_key = f'{model}/{lr}/{wd}/{da}'
+            run_key = f'{model}/{lr}/{wd}/{da}'
 
-                ap_curves_dict = add_curve_to_dict(event_acc, ap_curves_dict_key, run_key, ap_curves_dict)
-                loss_curves_dict = add_curve_to_dict(event_acc, loss_curves_dict_key, run_key, loss_curves_dict)
-                lr_curves_dict = add_curve_to_dict(event_acc, lr_curves_dict_key, run_key, lr_curves_dict)
+            ap_curves_dict = add_curve_to_dict(event_acc, ap_curves_dict_key, run_key, ap_curves_dict)
+            loss_curves_dict = add_curve_to_dict(event_acc, loss_curves_dict_key, run_key, loss_curves_dict)
+            lr_curves_dict = add_curve_to_dict(event_acc, lr_curves_dict_key, run_key, lr_curves_dict)
 
     return df, ap_curves_dict, loss_curves_dict, lr_curves_dict
 
@@ -133,13 +133,22 @@ def print_ap_table(df: pd.DataFrame) -> None:
     print('*' * 50)
 
 
+def print_best_result(df: pd.DataFrame, metric: str) -> None:
+    print('\nBest Result:\n')
+    print(df.iloc[df[metric].argmax()])
+
+
 if __name__ == '__main__':
     os.chdir('..')
     models_path = os.getcwd() + '/saved_models/'
     logs_path = os.getcwd() + '/logdir/'
 
     ap_table, ap_curves, loss_curves, lr_curves = parse_results(models_path, logs_path)
+
     print_ap_table(ap_table)
+
     generate_figure('AP', ap_curves)
     generate_figure('Mean Loss', loss_curves)
     generate_figure('Learning Rate', lr_curves)
+
+    print_best_result(ap_table, 'AP')
